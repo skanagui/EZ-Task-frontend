d5da0fef215d90fdac7c331033dce41055f7d4e9
skanagui <s.kanagui@gmail.com>
2020-11-09 23:05:56 -0500

    light psuedo

---
+++ src/index.js
@@ -0,0 +1,39 @@
+
+
+/****** Fetch Requests ********* */
+// Cusines 
+//Recipes 
+//Ingredients 
+
+
+/****** Render Functions for Get Requests ********* */
+//Cuisines - maybe h3 tag 
+//Recipes (Card) -Images, Description, Ingredients 
+//Stretch: Shopping list 
+
+
+/****** Click Handlers********* */
+//Cuisine Buttons
+//Recipe Buttons 
+
+/****** Submit  Handlers********* */
+//Submit to favorites 
+//Edit?
+//Delete?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
